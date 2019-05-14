90336aa472a732da34a67c01af982e8b9fc8bf46
DamoDimitrov <8mileisthebest>
2019-05-14 19:43:21 +0300

    firstTry

---
+++ src/JavaAdvanced/StacksAndQueues/Lab/BrowserHistory.java
@@ -0,0 +1,13 @@
+package JavaAdvanced.StacksAndQueues.Lab;
+
+import java.util.Scanner;
+
+public class BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = 5;
+
+        System.out.println(a);
+    }
+}
