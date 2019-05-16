4dbe32b8894690cba27f9cbea9cab9acbd46cfbb
DamoDimitrov <8mileisthebest>
2019-05-16 09:19:10 +0300

    Advanced course update

--- src/JavaAdvanced/StacksAndQueues/Lab/BrowserHistory.java
+++ src/JavaAdvanced/StacksAndQueues/Lab/BrowserHistory.java
@@ -6,8 +6,10 @@ public class BrowserHistory {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int a = 5;
+        String input = scanner.nextLine();
 
-        System.out.println(a);
+        while (!"Home".equals(input)) {
+
+        }
     }
 }
