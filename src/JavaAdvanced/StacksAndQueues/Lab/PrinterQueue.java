e72da4753dc6e4ea79f439d82f883fccdd1fc57c
DamoDimitrov <8mileisthebest>
2019-05-17 11:40:26 +0300

    last task

--- src/JavaAdvanced/StacksAndQueues/Lab/PrinterQueue.java
+++ src/JavaAdvanced/StacksAndQueues/Lab/PrinterQueue.java
@@ -1,4 +1,29 @@
 package JavaAdvanced.StacksAndQueues.Lab;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        ArrayDeque<String> files = new ArrayDeque<>();
+
+        while (!"print". equals(input)) {
+            if (!"cancel".equals(input)) {
+                files.offer(input);
+            } else {
+                if (files.size() == 0) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    System.out.println(String.format("Canceled %s", files.pop()));
+                }
+            }
+            input = scanner.nextLine();
+        }
+        for (String file:files) {
+            System.out.println(file);
+        }
+    }
 }
