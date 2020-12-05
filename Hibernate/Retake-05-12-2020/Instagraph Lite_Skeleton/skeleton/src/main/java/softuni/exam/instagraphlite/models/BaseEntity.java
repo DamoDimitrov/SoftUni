57673ea4b2ea075182b60b1e4291c126f0f78205
DamoDimitrov <damyan.v.dimitrov@gmail.com>
2020-12-05 15:22:33 +0200

    completed database tables and relations

--- Hibernate/Retake-05-12-2020/Instagraph Lite_Skeleton/skeleton/src/main/java/softuni/exam/instagraphlite/models/BaseEntity.java
+++ Hibernate/Retake-05-12-2020/Instagraph Lite_Skeleton/skeleton/src/main/java/softuni/exam/instagraphlite/models/BaseEntity.java
@@ -1,6 +1,21 @@
 package softuni.exam.instagraphlite.models;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
+@MappedSuperclass
 public abstract class BaseEntity {
+    private int id;
+
+    public BaseEntity() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- Hibernate/Retake-05-12-2020/Instagraph Lite_Skeleton/skeleton/src/main/java/softuni/exam/instagraphlite/models/Picture.java
+++ Hibernate/Retake-05-12-2020/Instagraph Lite_Skeleton/skeleton/src/main/java/softuni/exam/instagraphlite/models/Picture.java
@@ -1,4 +1,53 @@
 package softuni.exam.instagraphlite.models;
 
-public class Picture {
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "pictures")
+public class Picture extends BaseEntity{
+
+    private String path;
+    private double size;
+    private Set<User> users;
+    private Set<Post> posts;
+
+    public Picture() {
+    }
+
+    @Column
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Column
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    @OneToMany(mappedBy = "profilePicture")
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    @OneToMany(mappedBy = "picture")
+    public Set<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Set<Post> posts) {
+        this.posts = posts;
+    }
 }
